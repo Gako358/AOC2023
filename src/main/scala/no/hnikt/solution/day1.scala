985d0c730d99879c00d0988b6259deee5e80cc53
merrinx <gako358@outlook.com>
2023-12-01 11:41:47 +0100

    update scala code

--- src/main/scala/no/hnikt/solution/day1.scala
+++ src/main/scala/no/hnikt/solution/day1.scala
@@ -2,47 +2,48 @@ package no.hnikt.solution
 
 import scala.io.Source
 
-object Day1 {
-  def main(args: Array[String]): Unit = {
-    val lines = Source.fromFile("src/main/scala/no/hnikt/solution/day1.txt").getLines().toList
-    // val lines = List(
-    //   "two1nine",
-    //   "eightwothree",
-    //   "abcone2threexyz",
-    //   "xtwone3four",
-    //   "4nineeightseven2",
-    //   "zoneight234",
-    //   "7pqrstsixteen"
-    // )
-    val sum = calculateCalibrationSum(lines)
-    println(s"The sum of calibration values is: $sum")
+object Calibration {
+  val stringDigitReprs = Map(
+    "one" -> 1,
+    "two" -> 2,
+    "three" -> 3,
+    "four" -> 4,
+    "five" -> 5,
+    "six" -> 6,
+    "seven" -> 7,
+    "eight" -> 8,
+    "nine" -> 9
+  )
+
+  val digitReprs = stringDigitReprs ++ (0 to 9).map(i => i.toString -> i)
+
+  def solve(input: String): String = {
+    val digitReprRegex = digitReprs.keysIterator.mkString("|").r
+
+    def lineToCoordinates(line: String): Int = {
+      val matchesIter = for {
+        lineTail <- line.tails
+        oneMatch <- digitReprRegex.findPrefixOf(lineTail)
+      } yield oneMatch
+
+      val matches = matchesIter.toList
+      val firstDigit = digitReprs(matches.head)
+      val lastDigit = digitReprs(matches.last)
+      s"$firstDigit$lastDigit".toInt
+    }
+
+    val result = input.linesIterator
+      .map(lineToCoordinates(_))
+      .sum
+    result.toString()
   }
 
-  def calculateCalibrationSum(calibrationDocument: List[String]): Int = {
-    val numberMap = Map(
-      "zero" -> '0',
-      "one" -> '1',
-      "two" -> '2',
-      "three" -> '3',
-      "four" -> '4',
-      "five" -> '5',
-      "six" -> '6',
-      "seven" -> '7',
-      "eight" -> '8',
-      "nine" -> '9'
-    )
-    val numberPattern = (numberMap.keys.mkString("|") + "|\\d").r
-    calibrationDocument.map { line =>
-      val numbers = numberPattern.findAllIn(line).toList
-      if (numbers.nonEmpty) {
-        val firstNumber =
-          numberMap.getOrElse(numbers.head, numbers.head.head).asDigit
-        val lastNumber =
-          numberMap.getOrElse(numbers.last, numbers.last.head).asDigit
-        firstNumber * 10 + lastNumber
-      } else {
-        0
-      }
-    }.sum
+  def main(args: Array[String]): Unit = {
+    val lines = Source
+      .fromFile("src/main/scala/no/hnikt/solution/day1.txt")
+      .getLines()
+      .toList
+    val soln = solve(lines.mkString("\n"))
+    println(s"Solution: $soln")
   }
 }
